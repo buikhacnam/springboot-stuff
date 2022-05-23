400461b49db0bde83b38481b0f705383c9e9dd88
Bui Nam <buikhacnam11@gmail.com>
2022-05-23 09:37:02 +0700

    check if the player is existed before import

--- src/main/java/com/buinam/schedulemanger/controller/PlayerController.java
+++ src/main/java/com/buinam/schedulemanger/controller/PlayerController.java
@@ -25,17 +25,34 @@ public class PlayerController {
     public List<Player> importExcelFile(@RequestParam("file") MultipartFile files) throws IOException {
         List<Player> players = new ArrayList<>();
 
+        List<Player> existingPlayers = playerRepository.findAll();
+
+        List<String> names = new ArrayList<>();
+        List<String> clubs = new ArrayList<>();
+        existingPlayers.forEach(player -> {
+            names.add(player.getPlayerName());
+            clubs.add(player.getClub());
+        });
+
+
+
         XSSFWorkbook workbook = new XSSFWorkbook(files.getInputStream());
 
         // read player data from excel file sheet 1
         XSSFSheet worksheet = workbook.getSheetAt(0);
         for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) {
             if (worksheet.getRow(i) != null && worksheet.getRow(i).getCell(0) != null && worksheet.getRow(i).getCell(1) != null) {
-                Player player = new Player();
-                player.setPlayerName(worksheet.getRow(i).getCell(0).getStringCellValue());
-                player.setClub(worksheet.getRow(i).getCell(1).getStringCellValue());
-                player.setShirtNumber((int) worksheet.getRow(i).getCell(2).getNumericCellValue());
-                players.add(player);
+                String name = worksheet.getRow(i).getCell(0).getStringCellValue();
+                String club = worksheet.getRow(i).getCell(1).getStringCellValue();
+                int shirtNumber = (int) worksheet.getRow(i).getCell(2).getNumericCellValue();
+
+                if (!names.contains(name) && !clubs.contains(club)) {
+                    Player player = new Player();
+                    player.setPlayerName(name);
+                    player.setClub(club);
+                    player.setShirtNumber(shirtNumber);
+                }
+
             }
         }
 
